1b497b2b3054dbda83d221141f4e687274989336
Mert Unaler <Mert.Unaler@logo.com.tr>
2024-02-18 17:42:26 +0300

    project structure created

---
+++ RateLimiter/Bucket/Bucket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RateLimiter.Bucket
+{
+    public class Bucket
+    {
+        public TokenBucketModel GetBucket()
+        {
+            //TODO: CHECK CACHE IF PRESENT RETURN ELSE CREATE NEW ONE
+
+            return null;
+        }
+
+    }
+}
---
+++ RateLimiter/Bucket/TokenBucketModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RateLimiter.Bucket
+{
+    public sealed class TokenBucketModel
+    {
+        public TokenBucketModel(int rate, int bucketSize)
+        {
+            Rate = rate;
+            BucketSize = bucketSize;
+        }
+
+        public Guid BucketId { get; set; } = Guid.NewGuid();
+        public int Rate { get; set; } = 10;
+        public int BucketSize { get; set; } = 3;
+        public DateTime LastRefillTime { get; set; } = DateTime.UtcNow;
+
+    }
+}
